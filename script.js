6f11fdd65d3cbf65546541bbe9918f496d4294bc
mytvpro1 <tarifitmohammed49@gmail.com>
2026-03-29 00:17:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 function playChannel(url) {
     const video = document.getElementById('liveVideoPlayer');
-    
-    // سطر إظهار الفيديو
     video.style.display = "block";
 
+    // ربط الرابط بالـ Worker الخاص بك لتخطي الحماية وتجنب الشاشة السوداء
+    const proxyUrl = "https://mytvpro.mytvpropagesdev.workers.dev/?url=" + encodeURIComponent(url);
+
     if (Hls.isSupported()) {
-        const hls = new Hls();
-        hls.loadSource(url);
+        const hls = new Hls({
+            xhrSetup: function (xhr, url) {
+                xhr.withCredentials = false; 
+            }
+        });
+        hls.loadSource(proxyUrl); 
         hls.attachMedia(video);
         hls.on(Hls.Events.MANIFEST_PARSED, function() {
-            video.muted = true; 
-            video.play();
+            video.muted = false; 
+            video.play().catch(e => console.log("Auto-play prevented"));
         });
+
+        // مراقبة الأخطاء لمعرفة سبب توقف الصورة
+        hls.on(Hls.Events.ERROR, function (event, data) {
+            if (data.fatal) {
+                console.error("HLS Fatal Error:", data.type);
+            }
+        });
+
     } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        video.src = url;
+        video.src = proxyUrl;
         video.play();
     }
 }
