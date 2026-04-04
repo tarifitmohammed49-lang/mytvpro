d91fc774bf6efb23088ff9dd8401122f4a2c0a57
mytvpro1 <tarifitmohammed49@gmail.com>
2026-04-05 00:10:11 +0200

    Update channel_data.js

--- channel_data.js
+++ channel_data.js
@@ -1,16 +1,16 @@
 const myChannels = [
   {
-    "name": "DAZN DE | Event 8",
+    "name": "DAZN DE 8",
     "logo": "https://mytvpro1.github.io/favicon.ico",
     "url": "http://s1219.x.smline.xyz:2082/plays/UFBKVWd5VDBuaDFDNzBWNjM3VkpuMzZVMC9YUk93ajRoLzgwUUVkaTBLNE84VWVvdUlUS2U4V1p0VjJ2Unova2ttWUdidFAvdXg2cUFXUXFCL2hsYkE9PQ==.ts"
   },
   {
-    "name": "DAZN DE | Event 9",
+    "name": "DAZN DE 9",
     "logo": "https://mytvpro1.github.io/favicon.ico",
     "url": "http://s1219.x.smline.xyz:2082/plays/WTUraG1La1ljdzBUWHFXVE9kOUVJd296VXN5MXZtUWFFYXFpSUp4QkxvbTlGbzlxcVBMQlN0OFEzNXc4NUZQSXRWUHNUKzF3QnpkME1sdHR6aEZYNUE9PQ==.ts"
   },
   {
-    "name": "DAZN DE | Event 10",
+    "name": "DAZN DE 10",
     "logo": "https://mytvpro1.github.io/favicon.ico",
     "url": "http://s1219.x.smline.xyz:2082/plays/cjVaNS82L0lnRW9yL25DSXoxY2FyS2NMakpod3QvRWVqOXFlNjI2RVN3SU45OElFbGc5SWFWVG9sZ2d4bi9jU2o3Y1ExZlVYdE9LTVVOK1BoWnhLbmc9PQ==.ts"
   }
