f9ac4a9cfbda6639af80be8dd1e0c277710433e1
mytvpro1 <tarifitmohammed49@gmail.com>
2026-04-05 17:04:44 +0200

    Delete channel_data.js

--- channel_data.js
+++
@@ -1,6 +0,0 @@
-var live_channels = [
-  {
-    "name": "AL JAZEERA (AUTO-PROXY)",
-    "url": "https://broad-cake-463d.mytvpropagesdev.workers.dev/?target=http://s1219.x.smline.xyz:2082/plays/SFpZalQ5U0czZ292ZG1ZdXkvVmpwZ25Ec1htbG4xSkp3Ym1TRDVjMk1MdHo5SUozK2hCL3E1cFRZanYzTXlmeXFpRVhQcDMvMXZKMWQ2WlhHS0VSTUE9PQ==.ts"
-  }
-];
